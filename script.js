40efc62d2fec1fb3ab204971afa8002ce65e4868
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2023-05-05 21:50:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Develper","Competitive Programmer"],
+    typeSpeed: 90,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
